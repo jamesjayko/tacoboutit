a73b87e78d58cae1fc05639d5fb4edb08b81a3fa
Shoblik <shoblik@yahoo.com>
2017-11-29 10:56:04 -0800

    initial commit with working google places API

---
+++ main.js
@@ -0,0 +1,45 @@
+
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+var map;
+var infowindow;
+
+function initMap() {
+    var irvine = {lat: 33.6846, lng: -117.8265};
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: irvine,
+        zoom: 15
+    });
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: irvine,
+        radius: 1000,
+        type: ['restaurant']
+    }, callback);
+}
+
+function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+        }
+    }
+}
+
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+}
