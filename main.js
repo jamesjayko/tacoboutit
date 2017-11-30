cbebba266de6f78684db3bffcbc9b2c1fd4a29a6
Shoblik <shoblik@yahoo.com>
2017-11-30 12:41:26 -0800

    added in the radius button need to make a couple of improvements

--- main.js
+++ main.js
@@ -72,6 +72,7 @@ var model = {
         }, 500);
     },
     loc: null,
+    searchRadius: 1200,
     geocode: function() {
         $.ajax({
             url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + model.loc + '&key=AIzaSyDmBiq2uv9zLd2A1G5KwCbSaUYhMwO6mJg',
@@ -89,9 +90,16 @@ var model = {
         })
     },
     handleZipcodeInput: function() {
+        if ($('#searchRadiusInput').val() !== '') {
+            model.searchRadius = Number($('#searchRadiusInput').val());
+        }
+        if ($('#zipcodeSearch').val() !== '') {
+            model.loc = $('#zipcodeSearch').val();
+            model.geocode();
+        } else {
+            view.initMap()
+        }
         $('.placesList  div').remove();
-        model.loc = $('#zipcodeSearch').val();
-        model.geocode();
     }
 
 
@@ -167,7 +175,7 @@ var view = {
         model.service = new google.maps.places.PlacesService(model.map);
         model.service.nearbySearch({
             location: model.searchLocation,
-            radius: 1500,
+            radius: model.searchRadius,
             keyword: ('taco+mexican'),
             type: ('restaurant'),
         }, view.callback);
