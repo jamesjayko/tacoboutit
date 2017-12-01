9249ef53b60ba61a8f39336cf45f68fcac034fc2
Shoblik <shoblik@yahoo.com>
2017-11-30 17:32:31 -0800

    finished cleaning up our code

--- main.js
+++ main.js
@@ -15,18 +15,15 @@ function initializeApp() {
 
 
 var model = {
-    currentTaco: null,
-    i: 0,
-    map: null,
+   i: 0,
     infoWindow: null,
     resultsArr: null,
     searchLocation: null,
     service: null,
     currentDate: new Date(),
+    loc: null,
+    searchRadius: 3000,
 
-    setCurrentTaco: function(data) {
-        this.currentTaco = data;
-    },
     imgAPICall: function(query, ele) {
         var ajaxOptions = {
             url: "https://www.googleapis.com/customsearch/v1",
@@ -47,12 +44,12 @@ var model = {
     },
     getPlaceDetails: function() {
         var first = true;
-        function scrapePlaceDetails() {
+        function getAdditionalPlaceDetails() {
             model.service.getDetails(
                 {
                     placeId: model.resultsArr[model.i].place_id,
                 },
-                function(place, status) {
+                function(place) {
                     console.log(place);
                     model.resultsArr[model.i].simonsData = place;
                     if (model.i === model.resultsArr.length - 1) {
@@ -67,17 +64,14 @@ var model = {
             if (first) {
                 first = false;
                 model.i = 0;
-                scrapePlaceDetails();
+                getAdditionalPlaceDetails();
             } else if (model.i > model.resultsArr.length - 1) {
                 model.i = 0;
-
             } else {
-                scrapePlaceDetails();
+                getAdditionalPlaceDetails();
             }
         }, 500);
     },
-    loc: null,
-    searchRadius: 3000,
     geocode: function() {
         $.ajax({
             url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + model.loc + '&key=AIzaSyDmBiq2uv9zLd2A1G5KwCbSaUYhMwO6mJg',
@@ -106,8 +100,6 @@ var model = {
         }
         $('.placesList  div').remove();
     }
-
-
 };
 
 
@@ -179,7 +171,6 @@ var view = {
     },
     
     initMap: function () {
-        // model.searchLocation = {lat: 33.6509, lng: -117.7441};
         model.map = new google.maps.Map(document.getElementById('map'), {
             center: model.searchLocation,
             zoom: 12,
@@ -225,9 +216,9 @@ var view = {
             radius: model.searchRadius,
             keyword: ('taco+mexican'),
             type: ('restaurant')
-        }, view.callback);
+        }, view.storeResultsAndCallMarkers);
     },
-    callback: function (results, status) {
+    storeResultsAndCallMarkers: function (results, status) {
         model.resultsArr = results;
         model.getPlaceDetails();
         if (status === google.maps.places.PlacesServiceStatus.OK) {
@@ -237,12 +228,10 @@ var view = {
         }
     },
     createMarker: function createMarker(place) {
-        var placeLoc = place.geometry.location;
         var marker = new google.maps.Marker({
             map: model.map,
             position: place.geometry.location,
             icon: "images/taco_purp_marker.png",
-            class: "marker"
         });
         $(".mapContainer > .loader").remove();
         
@@ -295,7 +284,6 @@ var view = {
         element.append(textTagsArray);
     },
     initList: function () {
-        console.log(model.resultsArr);
         for (var i = 0; i < model.resultsArr.length; i++) {
             var elementsList = [];
 
@@ -393,18 +381,7 @@ var controller = {
 
         $.ajax(getTacoOptions).then(controller.tacoDataObtained.bind(this));
     },
-    createTacoRecipe: function(){
-        var getTacoOptions = {
-            dataType: 'json',
-            method: 'get',
-            url: 'http://taco-randomizer.herokuapp.com/random/?full-taco=true',
-        };
-
-        $.ajax(getTacoOptions).then( controller.tacoDataObtained.bind(this) )
-    },
-
     tacoDataObtained: function(data) {
-        model.setCurrentTaco(data);
         let tacoName = this.getSpecificTacoName(data.name);
         let gleanedRecipe = this.gleanRecipe(data.recipe);
         let layersArray = [];
@@ -466,7 +443,6 @@ var controller = {
     },
 
     loadSearchTacoModal: function(){
-        // this.getLocation();
         view.showSearchModal();
     },
 
@@ -474,7 +450,7 @@ var controller = {
     tacoTuesdayCountdown: function (date) {
         if (date.getDay() !== 2) {
             date.setDate(date.getDate() + (2 + 7 - date.getDay()) % 7);
-            date.setHours(0,0,0)
+            date.setHours(0,0,0);
             // Set the date we're counting down to
             var countDownDate = new Date(date).getTime();
             // Update the count down every 1 second
