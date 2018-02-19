4a640f943f387088ffdc03a776b839eb85dc76de
jauxsh <joshua.jih.chang@gmail.com>
2018-02-19 13:51:12 -0800

    defaulting error image for err in img api call, added max-width to img container

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ var model = {
     searchRadius: 2414,
     playSounds: true,
 
-    imgAPICall: function(query, ele) {
+    imgAPICall: function (query, ele) {
         var ajaxOptions = {
             url: "https://www.googleapis.com/customsearch/v1",
             method: "GET",
@@ -35,21 +35,21 @@ var model = {
                 searchType: "image",
                 key: "AIzaSyDI49X7IObH6sgXDPUK5uSEBf2EWdCmrHc"
             },
-            error: function(data) {
-                console.log(data);
+            error: function () {
+                view.appendImg(ele, "https://i.imgur.com/iTuHMPB.png");
             }
         };
 
         $.ajax(ajaxOptions).then(controller.tacoImageFilter.bind(null, ele));
     },
-    getPlaceDetails: function() {
+    getPlaceDetails: function () {
         var first = true;
         function getAdditionalPlaceDetails() {
             model.service.getDetails(
                 {
                     placeId: model.resultsArr[model.i].place_id,
                 },
-                function(place) {
+                function (place) {
                     console.log(place);
                     model.resultsArr[model.i].simonsData = place;
                     if (model.i === model.resultsArr.length - 1) {
@@ -59,7 +59,7 @@ var model = {
                 }
             );
         }
-        var int = setInterval(function() {
+        var int = setInterval(function () {
             model.i++;
             if (first) {
                 first = false;
@@ -73,7 +73,7 @@ var model = {
 
         }, 500);
     },
-    geocode: function() {
+    geocode: function () {
         $.ajax({
             url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + model.loc + '&key=AIzaSyDmBiq2uv9zLd2A1G5KwCbSaUYhMwO6mJg',
             method: 'get',
@@ -89,7 +89,7 @@ var model = {
             },
         })
     },
-    handleZipcodeInput: function() {
+    handleZipcodeInput: function () {
         if ($('#searchRadiusInput').val() !== '') {
             let miles = Number($('#searchRadiusInput').val());
             let meters = miles * 1609.34;
@@ -119,20 +119,20 @@ var view = {
         $(".recipeModalBackHome").on("click", this.hideRecipeModalBack);
         $(".searchModalReturn").on("click", this.hideSearchModal);
         $('.searchModalExpandToggle').on('click', this.toggleSearchModalExpand);
-        $(".recipeModalReturn").on("click", this.flipRecipeModalToFront );
+        $(".recipeModalReturn").on("click", this.flipRecipeModalToFront);
         $(".recipeModalGetNew").on("click", controller.createTacoRecipe.bind(controller));
         $('.modalButton').on('click', this.btnClickSound);
         $('.zipcodeBtn').on('click', model.handleZipcodeInput);
         $('#homeImg').on('click', this.fadeout);
         $('.sfxBtn').on('click', this.toggleSounds);
     },
-    toggleSounds: function() {
+    toggleSounds: function () {
         if (model.playSounds) {
-            $('.sfxBtn').empty();   
+            $('.sfxBtn').empty();
             $('.sfxBtn').append('<i class="fas fa-volume-off"></i>');
             model.playSounds = false;
         } else {
-            $('.sfxBtn').empty();            
+            $('.sfxBtn').empty();
             $('.sfxBtn').append('<i class="fas fa-volume-up"></i>');
             model.playSounds = true;
         }
@@ -147,28 +147,28 @@ var view = {
         $(".recipeModalContainer").css("top", "0");
         view.btnClickSound();
     },
-    flipRecipeModalToFront: function(){
+    flipRecipeModalToFront: function () {
         $('.recipeModalContainer').css('transform', 'translate(-50%, 0) rotateY(0deg)');
         $('.recipeModalFront').show();
-        setTimeout( function(){
+        setTimeout(function () {
             $('.recipeModalBack').hide();
         }, 500)
     },
-    flipRecipeModalToBack: function() {
+    flipRecipeModalToBack: function () {
         $('.recipeModalContainer').css('transform', 'translate(-50%, 0) rotateY(180deg)');
         $('.recipeModalBack').show();
-        setTimeout( function(){
+        setTimeout(function () {
             $('.recipeModalFront').hide();
         }, 500)
     },
     hideRecipeModal: function () {
         $(".recipeModalContainer").attr("style", "top: -250%");
     },
-    hideRecipeModalBack: function(){
+    hideRecipeModalBack: function () {
         $(".recipeModalContainer").css({
-                top: '-250%',
-                transform: 'translate(-50%, 0) rotateY(180deg)'
-            });
+            top: '-250%',
+            transform: 'translate(-50%, 0) rotateY(180deg)'
+        });
     },
     showSearchModal: function () {
         $(".searchModalContainer").css("top", "0");
@@ -177,7 +177,7 @@ var view = {
     hideSearchModal: function () {
         $(".searchModalContainer").attr("style", "top: -250");
     },
-    toggleSearchModalExpand: function(){
+    toggleSearchModalExpand: function () {
         $('.searchModal').children().toggleClass('map-expand');
         var icon = $('.searchModalExpandToggle').children();
         icon.toggleClass('fa-caret-down');
@@ -215,7 +215,7 @@ var view = {
             icon: "images/taco_purp_marker.png",
         });
         $(".mapContainer > .loader").remove();
-        
+
         google.maps.event.addListener(marker, "click", function () {
             model.infoWindow.setContent(place.name);
             model.infoWindow.open(model.map, this);
@@ -227,24 +227,24 @@ var view = {
     appendImg: function (ele, imgLink) {
         ele.attr("src", imgLink);
     },
-    clearRecipeModalText: function(element){
+    clearRecipeModalText: function (element) {
         element.empty();
     },
-    addRecipeModalLinks: function(linksArray){
+    addRecipeModalLinks: function (linksArray) {
         let linkElements = [];
-        for (let i=0; i<linksArray.length; i++){
-            let linkElement = $('<p>',{
+        for (let i = 0; i < linksArray.length; i++) {
+            let linkElement = $('<p>', {
                 text: "Recipe for " + linksArray[i].name,
                 'class': 'recipeLinks',
             });
             // using closure to connect the links I'm making with the appropriate recipe object
-            (function() {
-                linkElement.on('click', openAndShowComponentRecipe.bind(view) );
+            (function () {
+                linkElement.on('click', openAndShowComponentRecipe.bind(view));
                 function openAndShowComponentRecipe() {
-                    this.clearRecipeModalText( $('.recipeTextBack p') );
-                    this.changeRecipeModalHeader( linksArray[i].name, $('.recipeTextBack h2') );
-                    let gleanedRecipe = controller.gleanRecipe( linksArray[i].recipe );
-                    this.addRecipeModalText( gleanedRecipe, $('.recipeTextBack p') );
+                    this.clearRecipeModalText($('.recipeTextBack p'));
+                    this.changeRecipeModalHeader(linksArray[i].name, $('.recipeTextBack h2'));
+                    let gleanedRecipe = controller.gleanRecipe(linksArray[i].recipe);
+                    this.addRecipeModalText(gleanedRecipe, $('.recipeTextBack p'));
                     this.flipRecipeModalToBack();
                 }
             })();
@@ -256,7 +256,7 @@ var view = {
     },
     addRecipeModalText: function (textArray, element) {
         let textTagsArray = [];
-        for (let i=0; i<textArray.length; i++) {
+        for (let i = 0; i < textArray.length; i++) {
             let textNode = $('<p>').text(textArray[i]);
             textTagsArray.push(textNode);
         }
@@ -285,7 +285,7 @@ var view = {
             imgContainer.append(img);
             elementsList.push(imgContainer);
             if (model.resultsArr[i].name.length > 24 && model.resultsArr[i].hasOwnProperty('photos')) {
-                
+
                 var storeName = controller.nameTruncate(model.resultsArr[i].name);
 
                 var name = $('<h2>').text(storeName).addClass('name makeMeSmaller');
@@ -324,18 +324,18 @@ var view = {
 
             if (elementsList.length > 1) {
                 $(newDiv).append(elementsList);
-                $(".placesList > .loader").remove();                
+                $(".placesList > .loader").remove();
                 $('.placesList').append(newDiv);
             }
 
         }
     },
-    tacoTuesdayTimer: function(d, h, m, s) {
+    tacoTuesdayTimer: function (d, h, m, s) {
         $("#tacoTuesday").empty();
-        let timer =  d + "d " + h + "h " + m + "m " + s + "s" + " 'til Taco Tuesday";
+        let timer = d + "d " + h + "h " + m + "m " + s + "s" + " 'til Taco Tuesday";
         $("#tacoTuesday").append(timer);
     },
-    tacoTuesdayAnnounce: function(str) {
+    tacoTuesdayAnnounce: function (str) {
         $("#tacoTuesday").empty();
         $("#tacoTuesday").text(str);
     }
@@ -346,34 +346,34 @@ var view = {
 //====================================================//
 
 var controller = {
-    nameTruncate: function(str, length, ending) {
+    nameTruncate: function (str, length, ending) {
         if (length == null) {
-          length = 24;
+            length = 24;
         }
         if (ending == null) {
-          ending = '...';
+            ending = '...';
         }
         if (str.length > length) {
-          return str.substring(0, length - ending.length) + ending;
+            return str.substring(0, length - ending.length) + ending;
         } else {
-          return str;
+            return str;
         }
-      },
-    getLocation: function() {
+    },
+    getLocation: function () {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(controller.showPosition);
         } else {
             //Geolocation is not supported by this browser, pull up a modal?
         }
     },
-    showPosition: function(position) {
+    showPosition: function (position) {
         model.searchLocation = {
             lat: position.coords.latitude,
             lng: position.coords.longitude
         };
         view.initMap();
     },
-    createTacoRecipe: function() {
+    createTacoRecipe: function () {
         var getTacoOptions = {
             dataType: "json",
             method: "get",
@@ -382,32 +382,32 @@ var controller = {
 
         $.ajax(getTacoOptions).then(controller.tacoDataObtained.bind(this));
     },
-    tacoDataObtained: function(data) {
+    tacoDataObtained: function (data) {
         let tacoName = this.getSpecificTacoName(data.name);
         let gleanedRecipe = this.gleanRecipe(data.recipe);
         let layersArray = [];
 
         view.changeRecipeModalHeader(tacoName, $('.recipeTextFront h2'));
         model.imgAPICall(tacoName, $(".recipeImage img"));
-        view.clearRecipeModalText( $('.recipeTextFront p'));
+        view.clearRecipeModalText($('.recipeTextFront p'));
 
-        if (data.base_layer){
+        if (data.base_layer) {
             layersArray.push(data.base_layer);
         }
-        if (data.condiment){
+        if (data.condiment) {
             layersArray.push(data.condiment);
         }
-        if (data.mixin){
+        if (data.mixin) {
             layersArray.push(data.mixin);
         }
-        if (data.shell){
+        if (data.shell) {
             layersArray.push(data.shell);
         }
         view.addRecipeModalLinks(layersArray);
         view.addRecipeModalText(gleanedRecipe, $('.recipeTextFront > p'));
     },
 
-    getSpecificTacoName: function(longName) {
+    getSpecificTacoName: function (longName) {
         let endPoint = longName.indexOf(",");
         if (endPoint === -1) {
             return longName;
@@ -416,7 +416,7 @@ var controller = {
         return shortName;
     },
 
-    tacoImageFilter: function(ele, data) {
+    tacoImageFilter: function (ele, data) {
         var qArray = data.items;
 
         for (var qI = 0; qI < qArray.length; qI++) {
@@ -426,15 +426,15 @@ var controller = {
         }
     },
 
-    gleanRecipe: function(recipe){
-        let gleanedRecipe = recipe.replace(/[#*-=]|\./g,'');
+    gleanRecipe: function (recipe) {
+        let gleanedRecipe = recipe.replace(/[#*-=]|\./g, '');
         gleanedRecipe = gleanedRecipe.split('\n');
 
-        for (let i=0; i<gleanedRecipe.length; i++){
-            if (gleanedRecipe[i] === ''){
+        for (let i = 0; i < gleanedRecipe.length; i++) {
+            if (gleanedRecipe[i] === '') {
                 gleanedRecipe.splice(i, 1);
                 i--;
-            } else if (gleanedRecipe[i].indexOf('(') !== -1){
+            } else if (gleanedRecipe[i].indexOf('(') !== -1) {
                 gleanedRecipe[i] = gleanedRecipe[i].substr(0, gleanedRecipe[i].indexOf('('));
             }
         }
@@ -442,7 +442,7 @@ var controller = {
         return gleanedRecipe;
     },
 
-    loadSearchTacoModal: function(){
+    loadSearchTacoModal: function () {
         view.showSearchModal();
     },
 
@@ -450,11 +450,11 @@ var controller = {
     tacoTuesdayCountdown: function (date) {
         if (date.getDay() !== 2) {
             date.setDate(date.getDate() + (2 + 7 - date.getDay()) % 7);
-            date.setHours(0,0,0);
+            date.setHours(0, 0, 0);
             // Set the date we're counting down to
             var countDownDate = new Date(date).getTime();
             // Update the count down every 1 second
-            var x = setInterval(function() {
+            var x = setInterval(function () {
                 // Get todays date and time
                 var now = new Date().getTime();
                 // Find the distance between now an the count down date
